b634f0fa169351454c755aca21c4c6b5e99285bf
Mustafa Fırat Polat <118307621+mfiratpolat@users.noreply.github.com>
2023-01-15 20:59:15 +0300

    242but

---
+++ firatbut/Debug/Middlewares/Third_Party/EdgeImpulse_242final_MachineLearning/edgeimpulse/edge-impulse-sdk/porting/ethos-core-driver/src/ethosu_driver.d
@@ -0,0 +1,2 @@
+Middlewares/Third_Party/EdgeImpulse_242final_MachineLearning/edgeimpulse/edge-impulse-sdk/porting/ethos-core-driver/src/ethosu_driver.o: \
+ ../Middlewares/Third_Party/EdgeImpulse_242final_MachineLearning/edgeimpulse/edge-impulse-sdk/porting/ethos-core-driver/src/ethosu_driver.c
